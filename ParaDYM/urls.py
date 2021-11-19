32d46d40545d11ece1bbbbcb91240ab6577a1586
Jerry <defineantoine@gmail.com>
2021-11-19 02:52:36 -0500

    Pre-Sprint 3 - Client Requested Changes

--- ParaDYM/urls.py
+++ ParaDYM/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ProjectSite/', include('ProjectSite.urls')),
+    path('', include('ProjectSite.urls')),
 ]
